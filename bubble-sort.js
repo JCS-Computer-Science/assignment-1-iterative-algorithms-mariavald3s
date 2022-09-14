cbe0392af8f8b2f4815eab7bf5a6cb5e52b3a596
Maria Valdes <15297502@edu.cbe.ab.ca>
2022-09-14 11:53:08 -0600

    selection sort done

--- bubble-sort.js
+++ bubble-sort.js
@@ -11,34 +11,23 @@ function bubbleSort(array) {
 
   let sortedArray = [];
 
-  for (var i = 0; i < array.length; i++) {
-    if (array[i] > array[i + 1]) {
-    //   sortedArray.splice(i, 0 ,array[i]);
-    //   console.log(sortedArray);
-      if (i == 0) {
-          sortedArray.splice(i, 0 ,array[i]);
-          console.log(sortedArray);
-          
-      } else {
-      sortedArray.splice((i + 1), 0 ,array[i]);
-      sortedArray.splice((i), 0 ,array[i + 1]);
-      console.log(sortedArray);
-    } }
-    else if (array[i] < array[i + 1]) {
-        if (i == 0) {
-            sortedArray.splice(i, 0 ,array[i]);
-            console.log(sortedArray);
-            
-        } else {
-        sortedArray.splice((i - 1), 0 ,array[i]);
-        console.log(sortedArray);
-    }
-    } 
-    else if (array[i] == array[i + 1]) {
-        sortedArray.splice(i, 0 ,array[i]);
-        console.log(sortedArray);
-    } 
-  }
+  // for (var i = 0; i < array.length; i++) {
+  //   // for (var insideIndex = 0; insideIndex < (array.length - insideIndex - 1); insideIndex++) {
+  //   //   if (array[insideIndex] > array[insideIndex + 1]){
+  //   //     sortedArray.splice(insideIndex, 1, array[insideIndex + 1]);
+  //   //     sortedArray.splice(insideIndex + 1, 0, array[insideIndex]);
+  //   //     console.log(sortedArray)
+  //   //   }
+  //   //   if (array[insideIndex] < array[insideIndex + 1]){
+  //   //     sortedArray.splice(insideIndex, 1, array[insideIndex]);
+  //   //     sortedArray.splice(insideIndex + 1, 0, array[insideIndex + 1]);
+  //   //     console.log(sortedArray)
+  //   //   }
+
+  // }
+  
+
+// }
 
 
   return sortedArray;
--- selection-sort.js
+++ selection-sort.js
@@ -1,21 +1,22 @@
 function selectionSort(array) {
-	/*
-	 ** Search through the whole array for the smallest element
-	 ** then swap it with the first element.
-	 ** Next, search through the whole array (except the first element)
-	 ** for the smallest, then swap it with the second element.
-	 ** Continue like this until you have only one element left.
-	 */
-	
-	 let sortedArray = {};
+  /*
+   ** Search through the whole array for the smallest element
+   ** then swap it with the first element.
+   ** Next, search through the whole array (except the first element)
+   ** for the smallest, then swap it with the second element.
+   ** Continue like this until you have only one element left.
+   */
 
-	for (var i = 0; i < array.length; i++) {
-        if (array[i]) {
+  let sortedArray = [];
 
-		}
-	}
+  while (array.length > 0) {
+    var minimum = Math.min(...array);
+    var minimumIndex = array.indexOf(minimum);
+    sortedArray.push(minimum);
+    array.splice(minimumIndex, 1);
+  }
 
-	return sortedArray;
+  return sortedArray;
 }
 
 module.exports = selectionSort;
