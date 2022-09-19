c799146c35b15355bf7885fe4afef6bb68f0c78c
Maria Valdes <15297502@edu.cbe.ab.ca>
2022-09-19 11:57:00 -0600

    working on binary search

--- binary-search.js
+++ binary-search.js
@@ -1,5 +1,5 @@
 function binarySearch(array, searchTerm) {
-	/*
+  /*
 	 ** Check the middle element, if it's higher than the searchTerm
 	 ** then check the element in between the start and the middle, otherwise
 	 ** check the element between the middle and the end
@@ -7,34 +7,31 @@ function binarySearch(array, searchTerm) {
 	 ** repeat the above process for the subset of the array
 	 ** keep repeating with smaller and smaller subsets until you find the searchTerm
 	 */
-	let result;
+  let result;
 
-	let arrayTerms;
-	arrayTerms = array.length;
+  let start = 0;
+  let end = array.length;
 
-
-	for (var i = 0; i < array.length; i++) {
-	// while (arrayTerms > 1) {
-		// arrayTerms = Math.floor(arrayTerms / 2);
-		if (array[arrayTerms] > searchTerm) {
-			array.filter(arrayObj => arrayObj > searchTerm)
-			// array.splice(arrayTerms, arrayTerms);
-		} else if (array[arrayTerms] < searchTerm) {
-			array.filter(arrayObj => arrayObj > searchTerm)
-			//delete zero to array terms
-			// array.splice(0, arrayTerms);
-		} else if (array[arrayTerms] == searchTerm) {
-			result = arrayTerms;
-		}
-		console.log(searchTerm);
-		console.log(arrayTerms);
-		console.log(result);
-		console.log(array);
-	// }
-	}
+  let arrayTerms = end;
 
 
-	return result;
+    while (array[arrayTerms] != searchTerm) {
+      start = Math.floor((start + end) / 2);
+
+      if (array[arrayTerms] > searchTerm) {
+		  start = arrayTerms;
+        // array.splice(arrayTerms, arrayTerms);
+      } else if (array[arrayTerms] < searchTerm) {
+		  end = arrayTerms;
+        //delete zero to array terms
+        // array.splice(0, arrayTerms);
+      } 
+	  else if (array[arrayTerms] == searchTerm) {
+        result = arrayTerms;
+      }
+    }
+
+  return result;
 }
 
 module.exports = binarySearch;
